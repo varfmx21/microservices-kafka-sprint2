ff60c8022580307c58066a321a0dc27bc45b217e
varfmx21 <luisvalderrabano9@gmail.com>
2026-04-20 17:02:30 -0600

    create logic for topics and partitions

--- kafkaExample/str-producer/src/main/java/com/lta/backend/services/StringProducerService.java
+++ kafkaExample/str-producer/src/main/java/com/lta/backend/services/StringProducerService.java
@@ -15,17 +15,34 @@ public class StringProducerService {
     public void sendMessage(String message){
 
         int partition;
-        String topic; // 1. unsc-topic, 2.- covenant-topic, 3.- flood-topic.
+        String topic; // 1. unsc-topic, 2.- covenant-topic, 3.- flood-topic, 4.- forerunner-response-topic.
 
-        if(message.contains("menu")){
-            partition = 1; // listener2 (procesa y responde)
+        if(message.contains("unsc")){
+            topic = "unsc-topic";
+            if (message.contains("chief")){
+                partition = 0;
+            } else if (message.contains("cortana")){
+                partition = 1;
+            }
+        } else if(message.contains("covenant")){
+            topic = "covenant-topic";
+            if (message.contains("prophet")) {
+                partition = 0;
+            }
+        } else if(message.contains("flood")){
+            topic = "flood-topic";
+            if (message.contains("gravemind")) {
+                partition = 0;
+            }
+        } else if(message.contains("forerunner")){
+            topic = "forerunner-response-topic";
         } else {
-            partition = 0; // listener1 (no responde)
+            topic = "none";
         }
 
 
         //kafkaTemplate.send("str-topic", partition, null, message)
-        kafkaTemplate.send("str-topic",partition,null, message).whenComplete((result,ex) -> {
+        kafkaTemplate.send(topic,partition,null, message).whenComplete((result,ex) -> {
             if(ex != null){
                 log.error("Error, al enviar el mensaje: {}",ex.getMessage());
             }
